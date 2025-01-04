0ce48de26e47d5502dab76addeb0a55503ca8ab6
bjyting <104615858+xytyyds@users.noreply.github.com>
2025-01-04 21:05:31 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -15,17 +15,25 @@ def data_generate(reader):
         if start:
             start = False
             continue
-        images.append("./data_pic/"+row[0])
+        images.append("./data_pic/" + row[0])
         labels.append([int(row[i + 1]) for i in range(14)])
-
+    
+    #数据按照训练集：验证集：测试集 = 8 : 1 : 1来划分
     images = np.array(images)
     labels = np.array(labels)
-    train_images = images[:80000]
-    train_labels = labels[:80000]
-    test_images = images[80000:]
-    test_labels = labels[80000:]
+    total_num = len(images)
+    train_num = int(total_num * 0.8)
+    val_num = int(total_num * 0.1)
+    test_num = total_num - train_num - val_num
 
-    return train_images, train_labels, test_images, test_labels
+    train_images = images[:train_num]
+    train_labels = labels[:train_num]
+    val_images = images[train_num:train_num + val_num]
+    val_labels = labels[train_num:train_num + val_num]
+    test_images = images[train_num + val_num:]
+    test_labels = labels[train_num + val_num:]
+
+    return train_images, train_labels, val_images, val_labels, test_images, test_labels
 
 
 class CheXDataset(Dataset):
@@ -38,12 +46,12 @@ class CheXDataset(Dataset):
                                                          torchvision.transforms.ToTensor(),
                                                          torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                                           std=[0.229, 0.224, 0.225])
-                                                         ])
+                                                         ])  #数据归一化处理
 
     def __len__(self):
         return len(self.images)
 
-    # 可以使用水平翻转做数据增强
+    # 使用水平翻转做数据增强
     def __getitem__(self, item):
         image = Image.open(self.images[item]).convert('RGB')
         image = self.transform(image)
